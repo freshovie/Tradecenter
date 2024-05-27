2e9dcc5d39511a356802be32aaa9d5936e8a616c
Egorke Ovie <egorkeovie@gmail.com>
2024-05-27 12:17:21 +0100

    upgrade

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,62 +1,125 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './footer.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./footer.scss";
+import {
+  ApplePay,
+  Cart,
+  Discover,
+  Logo2,
+  Mastercard,
+  Visa,
+} from "../../assets";
 
 const Footer = () => {
   return (
     <React.Fragment>
       <section className="footer">
-        <div className="footer-row">
-          <div className="footer-col">
-            <h4>Newsletter</h4>
-            <p>
-              Subscribe to our newsletter for a weekly dose
-              of news, updates, helpful tips, and
-              exclusive offers.
-            </p>
+        <div className="container">
+          <div className="footer-row">
+            <div className="footer-col text-center first">
+              <img src={Logo2} alt="" />
+              <p className="text-center">
+                Morbi cursus porttitor enim lobortis molestie. Duis gravida
+                turpis dui, eget bibendum magna congue nec.
+              </p>
+              <p className="text-center">
+                <span>(219) 555 - 0114</span> or <span>Proxy@gmail.com</span>
+              </p>
+            </div>
+            <div className="footer-container">
+            <div className="footer-col">
+              <h4>My Account</h4>
+              <ul className="links">
+                <li>
+                  <Link to="#">My Account</Link>
+                </li>
+                <li>
+                  <Link to="#">Order History</Link>
+                </li>
+                <li>
+                  <Link to="#">Shopping Cart</Link>
+                </li>
+                <li>
+                  <Link to="#">Wishlist</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Help</h4>
+              <ul className="links">
+                <li>
+                  <Link to="#">Contact</Link>
+                </li>
+                <li>
+                  <Link to="#">Faqs</Link>
+                </li>
+                <li>
+                  <Link to="#">Terms & Condition</Link>
+                </li>
+                <li>
+                  <Link to="#">Privacy Policy</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Proxy</h4>
+              <ul className="links">
+                <li>
+                  <Link to="#">About</Link>
+                </li>
+                <li>
+                  <Link to="#">Shop</Link>
+                </li>
+                <li>
+                  <Link to="#">Product</Link>
+                </li>
+                <li>
+                  <Link to="#">Track Order</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Categories</h4>
+              <ul className="links">
+                <li>
+                  <Link to="#">Fruit & Vegetables</Link>
+                </li>
+                <li>
+                  <Link to="#">Meat & Fish</Link>
+                </li>
+                <li>
+                  <Link to="#">Bread & Bakery</Link>
+                </li>
+                <li>
+                  <Link to="#">Beauty & Health</Link>
+                </li>
+              </ul>
+            </div>
+            </div>
           </div>
-          <div className="footer-col">
-            <h4>Info</h4>
-            <ul className="links">
-              <li><Link to="#">About Us</Link></li>
-              <li><Link to="#">Compressions</Link></li>
-              <li><Link to="#">Customers</Link></li>
-              <li><Link to="#">Service</Link></li>
-              <li><Link to="#">Collection</Link></li>
-            </ul>
+          <hr />
+          {/* <!-- Copyright --> */}
+          <div
+            className="p-3 foot-head"
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.05)",
+              display: "flex",
+              justifyContent: "space-between",
+              flexWrap: "wrap",
+            }}
+          >
+            <a className="text-body foot-head" href="https://mdbootstrap.com/">
+              Trade Center eCommerce © 2021. All Rights Reserved
+            </a>
+            <div className="payment">
+              <img src={ApplePay} alt="" />
+              <img src={Visa} alt="" />
+              <img src={Discover} alt="" />
+              <img src={Mastercard} alt="" />
+              <img src={Cart} alt="" />
+            </div>
           </div>
-          <div className="footer-col">
-            <h4>Parties</h4>
-            <ul className="links">
-              <li><Link to="#">Ntnaija</Link></li>
-              <li><Link to="#">Represent</Link></li>
-              <li><Link to="#">Customers Party</Link></li>
-              <li><Link to="#">Party Services</Link></li>
-              <li><Link to="#">Party Collection</Link></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Explore</h4>
-            <ul className="links">
-              <li><Link to="#">Free Designs</Link></li>
-              <li><Link to="#">Latest Designs</Link></li>
-              <li><Link to="#">Themes</Link></li>
-              <li><Link to="#">Popular Designs</Link></li>
-              <li><Link to="#">Art Skills</Link></li>
-              <li><Link to="#">New Uploads</Link></li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Legal</h4>
-            <ul className="links">
-              <li><Link to="#">Customer Agreement</Link></li>
-              <li><Link to="#">Privacy Policy</Link></li>
-              <li><Link to="#">GDPR</Link></li>
-              <li><Link to="#">Security</Link></li>
-              <li><Link to="#">Testimonials</Link></li>
-              <li><Link to="#">Media Kit</Link></li>
-            </ul>
-          </div>
+          {/* <!-- Copyright --> */}
         </div>
       </section>
     </React.Fragment>
